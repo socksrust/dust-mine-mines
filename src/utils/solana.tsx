20dab48d33d4cc0be02c94e50c52477acd8b813c
Guilherme Jabur <guilhermejabur@outlook.com>
2021-12-12 23:32:10 -0300

    add sol fix url

--- src/utils/solana.tsx
+++ src/utils/solana.tsx
@@ -202,8 +202,8 @@ export const sendCurrencyToTreasure = async ({ fromWallet, toast, toTokenAccount
     const r = await localStorage.getItem('r')
     console.log('3')
 
-    //const resp = await fetch(`https://bip-gamextwo.herokuapp.com/api/v1/transaction/${endpoint}`, {
-    const resp = await fetch(`http://localhost:3009/api/v1/transaction/${endpoint}`, {
+    const resp = await fetch(`https://bip-gamextwo.herokuapp.com/api/v1/transaction/${endpoint}`, {
+    //const resp = await fetch(`http://localhost:3009/api/v1/transaction/${endpoint}`, {
       body: `{"transactionId":"${signature}", "betValue":"${betValue}", "currency":"${currency}", "r":"${r}"}`,
       headers: {
         "Content-Type": "application/json"
@@ -261,8 +261,8 @@ export const sendCurrencyToTreasure = async ({ fromWallet, toast, toTokenAccount
   const r = await localStorage.getItem('r')
   console.log('3')
 
-  //const resp = await fetch(`https://bip-gamextwo.herokuapp.com/api/v1/transaction/${endpoint}`, {
-  const resp = await fetch(`http://localhost:3009/api/v1/transaction/${endpoint}`, {
+  const resp = await fetch(`https://bip-gamextwo.herokuapp.com/api/v1/transaction/${endpoint}`, {
+  //const resp = await fetch(`http://localhost:3009/api/v1/transaction/${endpoint}`, {
     body: `{"transactionId":"${signature}", "betValue":"${betValue}", "currency":"${currency}", "r":"${r}"}`,
     headers: {
       "Content-Type": "application/json"
