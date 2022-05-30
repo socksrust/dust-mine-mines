b909935aeda5a485f6b67aec6800d00d3b38a281
Yg-or <inygu@icloud.com>
2022-05-30 18:14:46 -0300

    remove decimals to dmc

--- src/pages/roulette/index.tsx
+++ src/pages/roulette/index.tsx
@@ -241,7 +241,7 @@ export default function Home() {
                   active={currentValue === amount}
                   onClick={() => setBetValue(amount)}
                 >
-                  {amount.toFixed(2)} ${currentCurrency}
+                  {amount >= 1 ? amount : amount.toFixed(2)} ${currentCurrency}
                 </AmountButton>
               ))
             }
