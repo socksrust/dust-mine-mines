0ac26080dc77bc5d7ab62bc7a94c1dd73eab0edc
Yg-or <inygu@icloud.com>
2022-05-30 17:36:26 -0300

    fix page extensions

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const withTM = require('next-transpile-modules')([
 /** @type {import('next').NextConfig} */
 module.exports = withTM({
   reactStrictMode: true,
+  pageExtensions: ['jsx', 'tsx'],
   webpack5: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
